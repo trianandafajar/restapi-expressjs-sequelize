60e784a0163f22e79638c121e3df471bec9f8862
trianandafajar <mail.trianandafajar@gmail.com>
2025-05-10 23:35:16 +0700

    Update Route

--- 13-rest-api-contact/src/routes/index.js
+++ 13-rest-api-contact/src/routes/index.js
@@ -1,17 +1,24 @@
 import express from "express";
 import userRouter from "./userRoute.js";
-import { errorrHandling } from "../controllers/errorHandlingController.js";
 import contactRouter from "./contactRoute.js";
+import { errorHandling } from "../controllers/errorHandlingController.js";
+
 const route = express.Router();
 
+// Routes
 route.use("/api", userRouter);
 route.use("/api", contactRouter);
-route.use("*", errorrHandling);
+
+// 404 handler
 route.use("*", (req, res) => {
   res.status(404).json({
     errors: ["Page Not Found"],
-    message: "Internal Server Error",
+    message: "Invalid Route",
     data: null,
   });
 });
+
+// Global error handler
+route.use(errorHandling);
+
 export default route;
